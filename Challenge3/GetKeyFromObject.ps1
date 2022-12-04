458a072d5e5c565348283a58b8092dc083909155
dheerajsahni2409 <dheerajsahni2409@users.noreply.github.com>
2022-12-04 21:30:01 +0530

    Create GetKeyFromObject.ps1

---
+++ Challenge3/GetKeyFromObject.ps1
@@ -0,0 +1,39 @@
+﻿param(
+    [Parameter(Mandatory=$True,Position=1)]
+    [string]$input1,
+    [Parameter(Mandatory=$True,Position=2)]
+    [string]$input2,
+    [Parameter(Mandatory=$True,Position=3)]
+    [string]$input3,    
+    [Parameter(Mandatory=$True,Position=4)]
+    [string]$input4,
+    [Parameter(Mandatory=$True,Position=5)]
+    [string]$key
+)
+
+function object ($input1,$input2,$input3,$input4,$key)
+{
+
+$object = [PSCustomObject]@{ $input1 = [PSCustomObject]@{ $input2 = [PSCustomObject]@{ $input3 = $input4 } } }
+
+    if ($key -eq "$input1")
+    {
+        return $object.$input1
+    }
+    elseif ($key -eq ("$input1.$input2"))
+    {
+        return $object.$input1.$input2
+    }
+    elseif ($key -eq ("$input1.$input2.$input3"))
+    {
+        return $object.$input1.$input2.$input3
+    }
+    else 
+    {
+        Write-Host "Key doesn't match"
+    }
+}
+
+$output = object $input1 $input2 $input3 $input4 $key
+
+$output | ConvertTo-Json -Depth 5
