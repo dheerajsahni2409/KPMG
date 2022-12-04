3345e83a51d3552074930f6587ae100b69e443cc
dheerajsahni2409 <119789305+dheerajsahni2409@users.noreply.github.com>
2022-12-04 21:31:20 +0530

    Update GetKeyFromObject.ps1

--- Challenge3/GetKeyFromObject.ps1
+++ Challenge3/GetKeyFromObject.ps1
@@ -1,4 +1,4 @@
-﻿param(
+param(
     [Parameter(Mandatory=$True,Position=1)]
     [string]$input1,
     [Parameter(Mandatory=$True,Position=2)]
@@ -11,29 +11,43 @@
     [string]$key
 )
 
+#Starting function to create object and retrieve value based on the provided key
 function object ($input1,$input2,$input3,$input4,$key)
 {
-
-$object = [PSCustomObject]@{ $input1 = [PSCustomObject]@{ $input2 = [PSCustomObject]@{ $input3 = $input4 } } }
-
+try{
+    #Declaring Object
+    $object = [PSCustomObject]@{ $input1 = [PSCustomObject]@{ $input2 = [PSCustomObject]@{ $input3 = $input4 } } }
+    
+    #Declaring IF/ELSE condition to check the key-value pairing
+    #Checking for 1st case with top level key
     if ($key -eq "$input1")
     {
         return $object.$input1
     }
+    #checking second case with next level key
     elseif ($key -eq ("$input1.$input2"))
     {
         return $object.$input1.$input2
     }
+    #checking second case with last level key
     elseif ($key -eq ("$input1.$input2.$input3"))
     {
         return $object.$input1.$input2.$input3
     }
+    #Declaring case for invalid key
     else 
     {
-        Write-Host "Key doesn't match"
+        Write-Host "Key doesn't match, please check the key again"
     }
 }
+catch
+{
+    Write-Host $_.Exception.Message
+}
+}
 
+#calling the function
 $output = object $input1 $input2 $input3 $input4 $key
 
+#converting the output in JSON format for readability
 $output | ConvertTo-Json -Depth 5
